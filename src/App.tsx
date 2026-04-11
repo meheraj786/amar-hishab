ba98b770988b30f5c24b8709aa905f0d68bb7f4e
meheraj786 <meherajhosen786@gmail.com>
2026-04-11 14:08:21 +0600

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
   if (loading) {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-white">
-        <div className="w-10 h-10 border-4 border-emerald-500 border-t-transparent rounded-full animate-spin mb-4"></div>
+        <div className="w-10 h-10 border-4 border-primary border-t-transparent rounded-full animate-spin mb-4"></div>
         <p className="font-bold text-slate-500 italic">
           আমার খাতা লোড হচ্ছে...
         </p>
